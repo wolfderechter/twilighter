02f951794c9680defbce843a05b70a46221fdbea
wolfez <wolfderechter@gmail.com>
2024-10-26 11:03:36 +0200

    Implement volume slider + refactor to whole tab brightness

--- popup.js
+++ popup.js
@@ -14,13 +14,49 @@ async function updateBrightness(event) {
     target: { tabId: tab.id },
     args: [brightness], // pass brightness to the function
     func: (brightness) => {
-      const screenShare = document.querySelector(
-        '[data-type="screen-sharing"]',
-      );
+      // TODO: what should we dim?
+      // a) the whole screen
+      const rootElement = document.documentElement;
+      if (rootElement)
+        rootElement.style.filter = `brightness(${brightness / 100})`;
 
-      if (screenShare) {
-        screenShare.style.filter = `brightness(${brightness / 100})`;
-      }
+      // b) all the 'video' elements on a page
+      // const mediaElements = document.querySelectorAll("video");
+      // mediaElements.forEach(element => {
+      //   element.style.filter = `brightness(${brightness / 100})`;
+      // });
+
+      // c) the teams screenshare element
+      // const screenShare = document.querySelector(
+      //   '[data-type="screen-sharing"]',
+      // );
+      // if (screenShare) {
+      //   screenShare.style.filter = `brightness(${brightness / 100})`;
+      // }
+    },
+  });
+}
+const volumeSlider = document.getElementById("volume");
+const volumeValue = document.getElementById("volumeValue");
+volumeValue.textContent = volumeSlider.value;
+
+volumeSlider.addEventListener("input", updateVolume);
+
+async function updateVolume(event) {
+  const volume = event.target.value;
+  volumeValue.textContent = volume;
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    args: [volume], // pass volume to the function
+    func: (volume) => {
+      const mediaElements = document.querySelectorAll("audio, video");
+      mediaElements.forEach((element) => {
+        element.volume = volume / 100;
+        console.log(element, volume);
+      });
     },
   });
 }
